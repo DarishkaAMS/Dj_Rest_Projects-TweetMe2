595202a5ff3a6a9ee34dad1d004affa7158c8905
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-09-29 06:38:01 +0300

    Add views + Update models

--- tweets/models.py
+++ tweets/models.py
@@ -5,3 +5,6 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True) - by default
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.content} {self.image}"
