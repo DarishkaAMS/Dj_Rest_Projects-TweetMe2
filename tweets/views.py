195a6386e3489fdabaf23709a74aa5448fc824cf
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-09-30 05:02:53 +0300

    Add json data

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -8,8 +8,24 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    COnsume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+    data = {
+    "id": tweet_id,
+    # "content": obj.content,
+    # "image_path": obj.image.url
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content} </h1>")
+        data['message'] = "Sorry. We have not found that page"
+        status = 404
+        # raise Http404
+
+    return JsonResponse(data, status = status)
+    # return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content} </h1>")
