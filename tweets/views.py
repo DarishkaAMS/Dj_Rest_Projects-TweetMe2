6c402f95c981cf6541c0e3a01aa408c7a8ca8893
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-16 07:09:37 +0200

    Add tweet_detail_view

--- tweets/views.py
+++ tweets/views.py
@@ -2,5 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_view(request, * args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse('<h1>Hello</h1>')
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    return HttpResponse(f'<h1>Hello</h1> {tweet_id}')
+
+
