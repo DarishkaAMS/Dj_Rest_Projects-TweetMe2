ba54e99afc3c2498c49b99e6e88b8cbfcb56083d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-18 06:07:30 +0200

    Adding raise Http404 @ tweet_detail_view

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+
+from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
@@ -7,6 +9,11 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    return HttpResponse(f'<h1>Hello</h1> {tweet_id}')
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+
+    return HttpResponse(f'<h1>Hello there</h1> {tweet_id} - {obj.content}')
 
 
