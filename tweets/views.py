f3d33f7e17695b80aee231b727c569d251b788cd
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-21 07:16:37 +0200

    Fixing Errors

--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
+from .serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -15,6 +16,14 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    data = request.POST or None
+    serializer = TweetSerializer(data)
+    if serializer.is_valid():
+        serializer.save()
+    return JsonResponse({}, status=400)
+
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
     user = request.user
     if not request.user.is_authenticated:
         user = None  # for the anonymous Users
