e2f71fbe295b211b7a1114470c5d3548b060f144
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-22 07:05:29 +0200

    Add TweetLike and Update Tweet

--- tweets/models.py
+++ tweets/models.py
@@ -8,7 +8,9 @@ User = settings.AUTH_USER_MODEL
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through='TweetLike')
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
@@ -23,3 +25,7 @@ class Tweet(models.Model):
             "likes": random.randint(0, 20)
         }
 
+class g(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
