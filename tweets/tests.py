ffd6092c59c208f2ea2ade803a5b78ecbbebd9be
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-25 06:38:45 +0200

    Set test_user_created & test_tweet_created

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Tweet
 
 # Create your tests here.
+User = get_user_model()
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Marishka', password='moo')
+
+    def test_user_created(self):
+        self.assertEqual(self.user.username, 'Marishka')
+        # self.assertEqual(self.user.username, '1')
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content='Test Tweet', user=self.user)
+        self.assertEqual(tweet_obj.id, 1)
+        self.assertEqual(tweet_obj.user, self.user)
