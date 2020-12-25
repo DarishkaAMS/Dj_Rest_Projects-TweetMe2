7936198f222627cd9b6b53b23099299531f98abc
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-25 06:49:44 +0200

    Add get_client & test_tweet_list

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from .models import Tweet
 
+from rest_framework.test import APIClient
+
+from .models import Tweet
 # Create your tests here.
 User = get_user_model()
 
@@ -17,3 +19,14 @@ class TweetTestCase(TestCase):
         tweet_obj = Tweet.objects.create(content='Test Tweet', user=self.user)
         self.assertEqual(tweet_obj.id, 1)
         self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='moo')
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get('/api/tweets/')
+        self.assertEqual(response.status_code, 200)
+        print(response.json())
