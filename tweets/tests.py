eb3489602a9704348449ed647f42d401a95f11d3
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-25 07:06:54 +0200

    Add test_action_retweet and fixing retweet status_code

--- tweets/tests.py
+++ tweets/tests.py
@@ -44,9 +44,12 @@ class TweetTestCase(TestCase):
 
     def test_action_unlike(self):
         client = self.get_client()
-        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'like'})
-        self.assertEqual(response.status_code, 200)
         response = client.post('/api/tweets/action/', {'id': 2, 'action': 'unlike'})
         self.assertEqual(response.status_code, 200)
         like_count = response.json().get('likes')
         self.assertEqual(like_count, 0)
+
+    def test_action_retweet(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'retweet'})
+        self.assertEqual(response.status_code, 201)
