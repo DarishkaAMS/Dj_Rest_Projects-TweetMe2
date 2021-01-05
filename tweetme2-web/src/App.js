d888fdd46168be13a2d828099963a3576aacf169
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-05 05:47:08 +0200

    Reformating Tweet view @ App.js

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -20,6 +20,13 @@ function loadTweets(callback){
     xhr.send()
 }
 
+function Tweet(props) {
+    const {tweet} = props
+    return <div className='col-10 mx-auto col-md-6' >
+    <p>{tweet.content}</p>
+    </div>
+}
+
 function App() {
     const [tweets, setTweets] = useState([])
 
@@ -42,8 +49,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-            {tweets.map((tweet, index) => {
-                return <li> {tweet.content} </li>
+            {tweets.map((item, index) => {
+                return <Tweet tweet={item}/>
             })}
         </p>
         <a
