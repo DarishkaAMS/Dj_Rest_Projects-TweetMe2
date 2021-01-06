d3dc6ee19dffd4bc4d8418c62679344fb7b00392
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-06 06:45:27 +0200

    Improved Action btn

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -23,7 +23,10 @@ export function TweetsList(pros) {
 export function ActionBtn(props){
     const {tweet, action} = props
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    return action.type === 'like' ? <button className={className}> {tweet.likes} Like Me </button> : null
+    const actionDisplay = action.display ? action.display : 'Action'
+    const display = action.type === 'like' ? `${tweet.likes} ${actionDisplay}` : actionDisplay
+    return <button className={className}> {display} </button>
+
 }
 export function Tweet(props) {
     const {tweet} = props
@@ -31,7 +34,8 @@ export function Tweet(props) {
     return <div className={className}>
         <p>{tweet.id} - {tweet.content}</p>
         <div className='btn btn-group'></div>
-            <ActionBtn tweet={tweet} action={{type:'like'}}/>
-            <ActionBtn tweet={tweet} action={{type:'unlike'}}/>
+            <ActionBtn tweet={tweet} action={{type:'like', display: 'Likes'}}/>
+            <ActionBtn tweet={tweet} action={{type:'unlike', display: 'Unlike'}}/>
+            <ActionBtn tweet={tweet} action={{type:'retweet', display: 'Retweet'}}/>
         </div>
 }
