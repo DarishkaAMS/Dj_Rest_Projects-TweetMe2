759c863eb444ee16e4a8bb404a0129faeeea2cc1
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-08 07:38:27 +0200

    Add STATICFILES_DIRS and STATIC_ROOT + static-root

--- TweetMe2/settings.py
+++ TweetMe2/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
 
 CORS_ALLOW_ALL_ORIGINS = True #any web-site has access to my API
 CORS_URLS_REGEX = r'^/api/.*$'
