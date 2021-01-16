96b576d3071c495a80e98d7149259c072d8966d6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-16 08:37:27 +0200

    Rendering Limited Tweet List by Username

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -9,6 +9,7 @@ export function TweetsComponent(props){
     const textAreaRef = React.createRef()
     const [newTweets, setNewTweets] = useState([])
 
+    const canTweet = props.canTweet === 'false' ? false : true
     const handleBackendUpdate = (response, status) => {
         // backend api response handler
         let tempNewTweets = [...newTweets]
@@ -29,14 +30,15 @@ export function TweetsComponent(props){
         textAreaRef.current.value = ''
     }
     return <div className={props.className}>
-        <div className='col-12 mb-3'>
-     <form onSubmit={handleSubmit}>
-        <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
-        </textarea>
-        <button type='submit' className='btn btn-primary my-3'> Tweet </button>
-    </form>
+        {canTweet === true && <div className='col-12 mb-3'>
+             <form onSubmit={handleSubmit}>
+                <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
+                </textarea>
+                <button type='submit' className='btn btn-primary my-3'> Tweet </button>
+            </form>
         </div>
-            <TweetsList newTweets={newTweets} />
+        }
+            <TweetsList newTweets={newTweets} {...props}/>
     </div>
 }
 
@@ -60,9 +62,9 @@ export function TweetsList(props) {
                     alert('There was an error')
             }
         }
-        apiTweetList(null, handleTweetListLookup)
+        apiTweetList(props.username, handleTweetListLookup)
         }
-     }, [tweetsInit, tweetsDidSet, setTweetsDidSet])
+     }, [tweetsInit, tweetsDidSet, setTweetsDidSet, props.username])
 
     const handleDidRetweet = (newTweet) => {
         const updateTweetsInit = [...tweetsInit]
