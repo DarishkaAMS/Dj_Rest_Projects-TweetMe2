cdb79dd944a856a2d292acc20c264ff772b2a62f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-22 07:16:25 +0200

    Add login_view @ accunts/views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
+
+
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
+    return render(request, "form.html", {"form": form})
