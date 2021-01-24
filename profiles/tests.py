2566ca4facc217f1e51556412d21f311bcb32f32
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-24 05:38:32 +0200

    Add setUp @ ProfileTestCase

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+from .models import Profile
 # Create your tests here.
+
+User = get_user_model()
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user_m = User.objects.create_user(username='Marishka', password='moo')
+        self.user_b = User.objects.create_user(username='Barishka', password='boo')
+
