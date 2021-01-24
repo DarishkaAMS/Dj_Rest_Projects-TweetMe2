a8765cbbe09426ef5d3ed3688bc9d9b7d19628af
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-24 19:01:14 +0200

    Add ProfileSerializer @ serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,3 +2,23 @@ from rest_framework import serializers
 
 from .models import Profile
 
+
+class ProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
+    follower_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'first_name',
+            'last_name',
+            'id',
+            'bio',
+            'location',
+            'follower_count',
+            'following_count',
+            'username'
+        ]
