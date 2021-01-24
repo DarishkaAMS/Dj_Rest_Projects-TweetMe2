2c8fa24382b63f66d213441d1242b28fd379b076
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-24 05:48:54 +0200

    Add test_following @ ProfileTestCase

--- profiles/tests.py
+++ profiles/tests.py
@@ -12,6 +12,13 @@ class ProfileTestCase(TestCase):
         self.user_m = User.objects.create_user(username='Marishka', password='moo')
         self.user_b = User.objects.create_user(username='Barishka', password='boo')
 
-    def test_profile_created(self):
+    def test_profile_created_via_signal(self):
         query_set = Profile.objects.all()
         self.assertEqual(query_set.count(), 2)
+
+    def test_following(self):
+        first_user = self.user_m
+        second_user = self.user_b
+        first_user.profile.followers.add(second_user)
+        query_set = second_user.following.filter(user=first_user)
+        self.assertTrue(query_set.exists())
