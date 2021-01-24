a4faae29f1a077dfb478e458dc37342c1e1ef8f6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-24 13:57:11 +0200

    User Feed Including Following

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -34,6 +34,20 @@ def tweet_list_view(request, *args, **kwargs):
     return Response(serializer.data, status=200)
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def tweet_feed_view(request, *args, **kwargs):
+    user = request.user
+    profiles = user.following.all()
+    followed_users_id = []
+    if profiles.exists():
+        followed_users_id = [x.user.id for x in profiles]
+    followed_users_id.append(user.id)
+    query_set = Tweet.objects.filter(user__id__in=followed_users_id).order_by("-timestamp")
+    serializer = TweetSerializer(query_set, many=True)
+    return Response(serializer.data, status=200)
+
+
 @api_view(['GET'])  # http method the client == POST
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     query_set = Tweet.objects.filter(id=tweet_id)
